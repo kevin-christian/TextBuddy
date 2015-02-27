a4c69794d1a9aa5e5311c0196793c41c4ef74686
Kevin Christian <kevin3christian@gmail.com>
2015-02-27 19:56:39 +0800

    SearchEmpty and SearchNotFound tests added

--- TextBuddyTest/TextBuddyTest.cpp
+++ TextBuddyTest/TextBuddyTest.cpp
@@ -23,6 +23,24 @@ namespace TextBuddyTest
 			Assert::AreEqual("on top of the tree", tb.searchText("the")[1].c_str());
 			Assert::AreEqual("jumped over the lazy dog", tb.searchText("the")[0].c_str());
 		}
+		
+		TEST_METHOD(SearchNotFound)
+		{
+			TextBuddy tb("mytextfile.txt");
+			tb.addText("Little Brown Fox");
+			tb.addText("jumped over the lazy dog");
+			tb.addText("white fox");
+			tb.addText("played with firefox");
+			tb.addText("on top of the tree");
+			Assert::AreEqual(0U, tb.searchText("cat").size());
+		}
+		
+		TEST_METHOD(SearchEmpty)
+		{
+			TextBuddy tb("mytextfile.txt");
+			Assert::AreEqual(0U, tb.searchText("something").size());
+		}
+
 		TEST_METHOD(SortTest) {
 			TextBuddy tb("mytextfile.txt");
 			tb.addText("Mango");
